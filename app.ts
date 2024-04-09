41f3b46fe5809bba57fbf970d064116b494de969
Ange Michel <angemicheluwizeye@gmail.com>
2024-04-09 21:58:48 +0000

    ft: add session

--- app.ts
+++ app.ts
@@ -1,10 +1,22 @@
 import express from "express";
+import session from "express-session";
+import passport from "passport";
 import { Request, Response } from "express";
 import router from "./src/routes/authRoutes";
-import passport from "passport";
+
 const app = express();
 require("./src/authentication/auth");
+
 app.use(express.json());
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use("/", router);
 
 export default app;
